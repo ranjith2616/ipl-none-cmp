1adf44b9d36e003d6ef4076a282d077274012931
Ranjith <ranjithkuamr2k1z2gmail.com>
2023-08-30 16:41:01 +0000

    IPL non Com

---
+++ src/components/LatestMatch/index.js
@@ -0,0 +1,15 @@
+import './index.css'
+
+const LatestMatch = props => {
+  const {latestMatchDetails} = props
+
+  console.log(latestMatchDetails)
+
+  return (
+    <div>
+      <h1> Hello</h1>
+    </div>
+  )
+}
+
+export default LatestMatch
---
+++ src/components/TeamMatches/index.js
@@ -0,0 +1,130 @@
+import Loader from 'react-loader-spinner'
+
+import {Component} from 'react'
+
+import LatestMatch from '../LatestMatch'
+
+import './index.css'
+
+class TeamMatches extends Component {
+  state = {teamObj: {}, isLoading: true, bgColor: '', newDataObj: {}}
+
+  componentDidMount() {
+    this.getTeamData()
+  }
+
+  getTeamData = async () => {
+    const {match} = this.props
+    const {params} = match
+    const {id} = params
+
+    const response = await fetch(`https://apis.ccbp.in/ipl/${id}`)
+    const data = await response.json()
+
+    const updateData = {
+      teamBannerUrl: data.team_banner_url,
+      latestMatchDetails: data.latest_match_details,
+      recentMatches: data.recent_matches,
+    }
+
+    const updateLatestDetails = this.updateLatestDetails(updateData)
+    this.changeBgColor(id)
+    const updateRecentMatches = this.updateRecentMatches(updateData)
+
+    const newData = {
+      latestMatchDetails: updateLatestDetails,
+      recentMatches: updateRecentMatches,
+    }
+
+    this.setState({teamObj: updateData, newDataObj: newData, isLoading: false})
+  }
+
+  updateRecentMatches = updateData => {
+    const result = updateData.recentMatches.map(each => ({
+      competingTeam: each.competing_team,
+      competingTeamLogo: each.competing_team_logo,
+      date: each.date,
+      firstInnings: each.first_innings,
+      manOfTheMatch: each.man_of_match,
+      matchStatus: each.match_status,
+      result: each.result,
+      secondInnings: each.second_innings,
+      umpires: each.umpires,
+      venue: each.venue,
+    }))
+
+    return result
+  }
+
+  updateLatestDetails = updateData => {
+    const updateLatestDetails = {
+      competingTeam: updateData.latestMatchDetails.competing_team,
+      competingTeamLogo: updateData.latestMatchDetails.competing_team_logo,
+      date: updateData.latestMatchDetails.date,
+      firstInnings: updateData.latestMatchDetails.first_innings,
+      manOfTheMatch: updateData.latestMatchDetails.man_of_match,
+      matchStatus: updateData.latestMatchDetails.match_status,
+      result: updateData.latestMatchDetails.result,
+      secondInnings: updateData.latestMatchDetails.second_innings,
+      umpires: updateData.latestMatchDetails.umpires,
+      venue: updateData.latestMatchDetails.venue,
+    }
+
+    return updateLatestDetails
+  }
+
+  changeBgColor = id => {
+    switch (id) {
+      case 'RCB':
+        this.setState({bgColor: 'rcb'})
+        break
+      case 'KKR':
+        this.setState({bgColor: 'kkr'})
+        break
+      case 'KXP':
+        this.setState({bgColor: 'kxp'})
+        break
+      case 'CSK':
+        this.setState({bgColor: 'csk'})
+        break
+      case 'RR':
+        this.setState({bgColor: 'rr'})
+        break
+      case 'MI':
+        this.setState({bgColor: 'mi'})
+        break
+      case 'SRH':
+        this.setState({bgColor: 'srh'})
+        break
+      case 'DC':
+        this.setState({bgColor: 'dc'})
+        break
+
+      default:
+        break
+    }
+  }
+
+  render() {
+    const {teamObj, isLoading, bgColor, newDataObj} = this.state
+    const {latestMatchDetails, recentMatches} = newDataObj
+    const {teamBannerUrl} = teamObj
+
+    return (
+      <div>
+        {isLoading ? (
+          <div data-testid="loader" className="loader">
+            {' '}
+            <Loader type="Oval" color="#1e293b" height={50} width={50} />{' '}
+          </div>
+        ) : (
+          <div className={`selected-team-bg-container ${bgColor}`}>
+            <img src={teamBannerUrl} alt="team banner" className="banner-img" />
+            <LatestMatch latestMatchDetails={latestMatchDetails} />
+          </div>
+        )}
+      </div>
+    )
+  }
+}
+export default TeamMatches
